ee52fdf5f617051d9df60fea3b58b8f7044a872a
arpandiablo <unitedarpan@gmail.com>
2022-06-30 16:56:22 +0545

    Test

--- add/sum.java
+++ add/sum.java
@@ -1,8 +1,17 @@
 package add;
+import java.util.Scanner;
+
 
 public class sum {
-    public static void main(String arg[]){
-        int n1,n2,sum
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n1,n2,sum;
+        System.out.print("Enter first number : ");  
+        n1= sc.nextInt();
+        System.out.print("Enter second number : ");
+        n2= sc.nextInt();
+        sum=n1+n2;
+        System.out.println("The sum is " +sum);
     }
 }
 
